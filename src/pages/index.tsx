adfd4360a1c0999e926cad85a7f3ec0eb82e4009
Nathalia Campos <nathalia@4intelligence.com.br>
2021-02-27 20:57:58 -0300

    feat: create challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
@@ -20,7 +21,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
